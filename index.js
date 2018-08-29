cc49bede2484995054d9b673dd20b4fd0b86b45c
Sahota <Harpreet.Sahota@kantar.com>
2018-08-29 13:03:42 +0530

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+const readline = require('readline');
+const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
+
+// youtube video ids array
+let ids = ['1W5BA0lDVLM'];
+let title = '';
+let counter = ids.length - 1;
+
+function init() {
+
+    let id = ids[counter];
+    counter--;
+
+    let stream = ytdl(id, {
+        quality: 'highestaudio',
+    });
+
+    ytdl.getInfo(id, function (e, i) {
+        console.log('-------> ', i.title);
+        title = i.title;
+        initiateDownload();
+    });
+
+    function initiateDownload() {
+        let start = Date.now();
+        ffmpeg(stream)
+            .audioBitrate(320)
+            // .save(`${__dirname}/downloads/${title}_${id}.mp3`)
+            .save(`${__dirname}/downloads/${id}.mp3`)
+            .on('progress', (p) => {
+                readline.cursorTo(process.stdout, 0);
+                process.stdout.write(`${p.targetSize}kb downloaded`);
+            })
+            .on('end', () => {
+                console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s, Next id: ${counter}`);
+                if (counter >= 0) {
+                    init();
+                }
+            });
+    }
+}
+
+init();
